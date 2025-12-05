954bd3a4767824931300d715401d8becf485fab3
momoji123 <77244643+momoji123@users.noreply.github.com>
2025-12-05 11:55:02 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,52 @@
 @echo off
+setlocal enabledelayedexpansion
+set isSuccess=true
 
-echo "build desktop app executor"
 
-pyinstaller desktop_app.py --name PrismTask --add-data index.html:. --add-data Assets:Assets --add-data css:css --add-data js:js --icon=favicon.ico --distpath .\dist\desktop_app --onedir
 
-echo "build user manager app executor"
+call venv\Scripts\activate.bat
+if errorlevel 1 (
+    echo Failed to activate venv
+    set isSuccess=false
+)
 
-pyinstaller user_manager.py --name user_manager --distpath .\dist\desktop_app\run_desktop --onefile --console
+if "%isSuccess%"=="true" (
+	echo "build desktop app executor"
+	
+	rmdir /S /Q ".\dist\desktop_app"
 
-echo "build db migration app executor"
+	pyinstaller desktop_app.py --name PrismTask --add-data index.html:. --add-data Assets:Assets --add-data css:css --add-data js:js --icon=favicon.ico --distpath .\dist\desktop_app --onedir
+	if errorlevel 1 (
+		echo "Failed to build PrismTask (main)"
+		set isSuccess=false
+	)
+)
 
-pyinstaller SQLite3_Migration.py --name db_migration --distpath .\dist\desktop_app\run_desktop --onefile --console
+if "%isSuccess%"=="true" (
+	echo "build user manager app executor"
+
+	pyinstaller user_manager.py --name user_manager --distpath .\dist\desktop_app\PrismTask --onefile --console
+	if errorlevel 1 (
+		echo "Failed to build user_manager"
+		set isSuccess=false
+	)
+)
+
+if "%isSuccess%"=="true" (
+	echo "Copying Licence Files"
+	robocopy third_party_licenses .\dist\desktop_app\PrismTask\third_party_licenses /E
+
+	copy LICENSE .\dist\desktop_app\PrismTask
+	copy NOTICE .\dist\desktop_app\PrismTask
+	copy "README (For Dist).md" .\dist\desktop_app\PrismTask\README.md
+	
+	echo "Renaming Result Folder"
+
+	for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd"') do set TODAY=%%i
+	
+	pushd .\dist\desktop_app
+	ren PrismTask PrismTask-V1.0.!TODAY!
+	popd
+)
+
+pause
